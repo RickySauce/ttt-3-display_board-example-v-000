3122932db1355edbdbc58af135fb3ca9c7df84d1
Eric DeSimone <eric.desimone@cortland.edu>
2018-01-05 11:09:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,8 +10,8 @@ def display_board
   cell_8 = "  "
   cell_9 = "  "
   puts " #{cell_1}| #{cell_2}|#{cell_3}"
-  puts "----------- "
+   "----------- "
   puts " #{cell_4}| #{cell_5}|#{cell_6}"
-  puts "----------- "
+ "----------- "
   puts " #{cell_7}| #{cell_8}|#{cell_9}"
 end
